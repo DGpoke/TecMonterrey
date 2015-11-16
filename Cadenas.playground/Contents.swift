ac336a03a4e7e5b0c957db408e9837766be707d0
DGpoke <DGpoke@DGit.local>
2015-11-15 22:15:40 -0500

    x
    x

---
+++ Cadenas.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Cadena de escape, utilizamos la \n y con esto salta la linea o \t para un tab
+print("Lista de deportes: \n1. Basketball \n2. Soccer \n3. Natacion")
+
+//Cambiando por un emoji, entramos a menu Edit y hasta abajo Emoji
+print("Lista de deportes: \n1. 🏀 \n2. ⚽️ \n3. 🏊")
---
+++ operadoresU.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Operadores unarios
+
+var numeroDeVidas = 5
+
+numeroDeVidas += numeroDeVidas + 1
+numeroDeVidas
+//Post incrementar
+numeroDeVidas++
+numeroDeVidas
+//Pre incrementar
+++numeroDeVidas
+//Post decremento
+numeroDeVidas--
+//Pre decremento
+--numeroDeVidas
+
+//Cambiando el signo
+-numeroDeVidas
+
+var esDeDia : Bool = true
+
+//Negando la variable
+!esDeDia
+
+
+let operacion = 300 - 201 * 9 % 6 / 8
